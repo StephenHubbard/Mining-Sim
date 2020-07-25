19a618c9310402847c71587f00734caa9045279e
Stephen Hubbard <hubbard_stephen@yahoo.com>
2020-07-25 12:14:42 -0600

    mineral gen

---
+++ Assets/Imported Assets/EasyParallax/SpriteDuplicator.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+namespace EasyParallax
+{
+/**
+ * Creates copies of this object and arranges them seamlessly, so that one is right next to the other.
+ */
+[RequireComponent(
+    typeof(SpriteRenderer))] //Make sure we have a SpriteRenderer, because we need the width of the sprite
+public class SpriteDuplicator : MonoBehaviour
+{
+    /**
+     * The number of copies of the object that we will have at any given time, including this object
+     */
+    [SerializeField] private int poolSize = 5;
+
+    /**
+     * An array of all current copies of this object, including this object
+     */
+    private Transform[] duplicatesPool;
+
+    /**
+     * Here we keep the width of the sprite, so we can later calculate where to place other objects
+     */
+    private float spriteWidth;
+
+    private void Start()
+    {
+        //First, we instantiate a number of objects that we will reuse. This is also called pooling. 
+        //We do this, because it's more efficient than creating and destroying objects over and over again.
+        duplicatesPool = new Transform[poolSize];
+
+        //Now, we need to get the width of our sprite, because we will position the objects based on how wide they are
+        spriteWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+
+        //Let's populate our array with enough items. First add this object, as it's part of the pool.
+        duplicatesPool[0] = transform;
+
+        var startingPos = transform.position;
+
+        //Next duplicate this and add the objects, until we fill our pool
+        for (var i = 1; i < poolSize; i++)
+        {
+            var position = new Vector2(startingPos.x + i * spriteWidth, startingPos.y);
+            duplicatesPool[i] = Instantiate(gameObject, position, Quaternion.identity, transform.parent).transform;
+            //It's very important to remove the sprite duplicator script from the copied object
+            //Otherwise we will get an infinite loop of sprite duplication
+            Destroy(duplicatesPool[i].GetComponent<SpriteDuplicator>());
+        }
+    }
+
+
+    private void Update()
+    {
+        //We need to check if any of our sprites has gone of screen. 
+        //We will reposition it to be on the right side if it is
+        foreach (var duplicate in duplicatesPool)
+        {
+            //Let's assume that 2 sprites are enough to fill the screen
+            if (duplicate.transform.position.x < -spriteWidth * 2)
+            {
+                //In order to reposition it, we need to get which sprite is the rightmost currently
+                var rightmostSprite = GetRightMostSprite();
+                
+                //Let's position it to the right of that sprite
+                var startingPos = rightmostSprite.position;
+                var position = new Vector2(startingPos.x + spriteWidth, startingPos.y);
+                duplicate.transform.position = position;
+            }
+        }
+    }
+
+    private Transform GetRightMostSprite()
+    {
+        var rightmostX = Mathf.NegativeInfinity;
+        Transform rightmostSprite = null;
+        foreach (var duplicate in duplicatesPool)
+        {
+            if (!(duplicate.position.x > rightmostX)) continue;
+            
+            rightmostSprite = duplicate;
+            rightmostX = duplicate.position.x;
+        }
+
+        return rightmostSprite;
+    }
+}
+}
--- Assets/Scripts/ProceduralGeneration.cs
+++ Assets/Scripts/ProceduralGeneration.cs
@@ -11,7 +11,7 @@ public class ProceduralGeneration : MonoBehaviour
     [SerializeField] GameObject dirt, grass, stone, diamond, ruby;
     [Range(0, 100)]
     [SerializeField] int seed;
-    
+
     // Cave Variables
     [SerializeField] int smoothCycles;
     private int[,] cavePoints;
@@ -23,6 +23,19 @@ public class ProceduralGeneration : MonoBehaviour
     [Range(0, 8)]
     public int threshold;
 
+    // Mineral Fields
+    private int diamondStarterInt = 0;
+    [Range(0, 10)]
+    public int spreadDiamondsMultiplier = 3;
+    [Range(0, 10)]
+    public int diamondRarity = 0;
+
+    private int rubyStarterInt = 0;
+    [Range(0, 10)]
+    public int spreadRubysMultiplier = 3;
+    [Range(0, 10)]
+    public int rubyRarity = 0;
+
     private void Awake()
     {
         seed = Random.Range(0, 1000000);
@@ -32,6 +45,18 @@ public class ProceduralGeneration : MonoBehaviour
     private void Start()
     {
         Generation();
+    }
+
+    private void Update()
+    {
+        if (diamondStarterInt < spreadDiamondsMultiplier)
+        {
+            GenDiamonds();
+        }
+        if (rubyStarterInt < spreadRubysMultiplier)
+        {
+            GenRubys();
+        }
     }
 
     private void GenerateCaves()
@@ -121,15 +146,14 @@ public class ProceduralGeneration : MonoBehaviour
             System.Random randChoice = new System.Random(seed.GetHashCode());
 
 
-            genHeight(z, height, totalStoneSpawnDistance);
-            genDepth(z);
-            genTopLevel(z, height, totalStoneSpawnDistance);
-            StartCoroutine(ExecuteAfterTime(1f, z));
+            GenHeight(z, height, totalStoneSpawnDistance);
+            GenDepth(z);
+            GenTopLevel(z, height, totalStoneSpawnDistance);
 
         }
     }
 
-    private void genTopLevel(int z, int height, int totalStoneSpawnDistance)
+    private void GenTopLevel(int z, int height, int totalStoneSpawnDistance)
     {
         if (cavePoints[z, height] == 1)
         {
@@ -144,14 +168,7 @@ public class ProceduralGeneration : MonoBehaviour
         }
     }
 
-    private IEnumerator ExecuteAfterTime(float time, int z)
-    {
-        yield return new WaitForSeconds(time);
-
-        genMinerals(z);
-    }
-
-    private void genDepth(int z)
+    private void GenDepth(int z)
     {
 
         for (int y = 0; y < depth; y++)
@@ -173,46 +190,95 @@ public class ProceduralGeneration : MonoBehaviour
 
     }
 
-    private void genMinerals(int z)
+    private void GenDiamonds()
     {
-        int spreadDiamondsMultiplier = 0;
-
-        for (int y = 0; y < depth; y++)
+        diamondStarterInt++;
+        for (int z = 0; z < width; z++)
         {
-            Vector3 mineralLocation = new Vector3(0, -y, z);
-            Collider[] getBlockType = Physics.OverlapSphere(mineralLocation, 0.1f);
-            if (getBlockType.Length > 0)
+            for (int y = 0; y < depth; y++)
             {
-                if (getBlockType[0].gameObject.GetComponent<Diamond>())
+                Vector3 mineralLocation = new Vector3(0, -y, z);
+                Collider[] getBlockType = Physics.OverlapSphere(mineralLocation, 0.1f);
+                if (getBlockType.Length > 0)
                 {
-                    Vector3 diamondLocation = getBlockType[0].gameObject.transform.position;
-                    Collider[] diamondIsTouching = Physics.OverlapSphere(diamondLocation, .6f);
-                    if (diamondIsTouching.Length > 0)
+                    if (getBlockType[0].gameObject.GetComponent<Diamond>())
                     {
-                        
-                        for (int i = 0; i < diamondIsTouching.Length; i++)
+                        Vector3 diamondLocation = getBlockType[0].gameObject.transform.position;
+                        Collider[] diamondIsTouching = Physics.OverlapSphere(diamondLocation, .6f);
+                        if (diamondIsTouching.Length > 0)
                         {
-                            if (!diamondIsTouching[i].gameObject.GetComponent<Diamond>())
+
+                            for (int i = 0; i < diamondIsTouching.Length; i++)
                             {
-                                GameObject newDiamond = Instantiate(diamond, diamondIsTouching[i].transform.position, Quaternion.identity);
-                                newDiamond.transform.parent = this.transform;
-                                Destroy(diamondIsTouching[i].gameObject);
-                                
+                                if (!diamondIsTouching[i].gameObject.GetComponent<Diamond>())
+                                {
+                                    bool shouldSpreadDiamond = false;
+                                    if (Random.Range(0, diamondRarity) == 1)
+                                    {
+                                        shouldSpreadDiamond = true;
+                                    }
+
+                                    if (shouldSpreadDiamond == true)
+                                    {
+                                        GameObject newDiamond = Instantiate(diamond, diamondIsTouching[i].transform.position, Quaternion.identity);
+                                        newDiamond.transform.parent = this.transform;
+                                        Destroy(diamondIsTouching[i].gameObject);
+                                    }
+
+                                }
                             }
                         }
                     }
                 }
             }
         }
-
-        //spreadDiamondsMultiplier++;
-        //if (spreadDiamondsMultiplier < 3)
-        //{
-        //    genMinerals(z);
-        //}
     }
 
-    private void genHeight(int z, int height, int totalStoneSpawnDistance)
+    private void GenRubys()
+    {
+        rubyStarterInt++;
+        for (int z = 0; z < width; z++)
+        {
+            for (int y = 0; y < depth; y++)
+            {
+                Vector3 mineralLocation = new Vector3(0, -y, z);
+                Collider[] getBlockType = Physics.OverlapSphere(mineralLocation, 0.1f);
+                if (getBlockType.Length > 0)
+                {
+                    if (getBlockType[0].gameObject.GetComponent<Ruby>())
+                    {
+                        Vector3 rubyLocation = getBlockType[0].gameObject.transform.position;
+                        Collider[] rubyIsTouching = Physics.OverlapSphere(rubyLocation, .6f);
+                        if (rubyIsTouching.Length > 0)
+                        {
+
+                            for (int i = 0; i < rubyIsTouching.Length; i++)
+                            {
+                                if (!rubyIsTouching[i].gameObject.GetComponent<Ruby>())
+                                {
+                                    bool shouldSpreadruby = false;
+                                    if (Random.Range(0, rubyRarity) == 1)
+                                    {
+                                        shouldSpreadruby = true;
+                                    }
+
+                                    if (shouldSpreadruby == true)
+                                    {
+                                        GameObject newRuby = Instantiate(ruby, rubyIsTouching[i].transform.position, Quaternion.identity);
+                                        newRuby.transform.parent = this.transform;
+                                        Destroy(rubyIsTouching[i].gameObject);
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void GenHeight(int z, int height, int totalStoneSpawnDistance)
     {
         for (int y = 0; y < height; y++)
         {
