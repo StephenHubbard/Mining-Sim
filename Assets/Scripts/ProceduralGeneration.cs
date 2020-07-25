1a54a70fe2d7a2663008ceda7ac30ecf0cc703c7
Stephen Hubbard <hubbard_stephen@yahoo.com>
2020-07-25 10:38:22 -0600

    gem gen

--- Assets/Scripts/ProceduralGeneration.cs
+++ Assets/Scripts/ProceduralGeneration.cs
@@ -8,15 +8,18 @@ public class ProceduralGeneration : MonoBehaviour
     [SerializeField] int depth;
     [SerializeField] float heightValue, smoothnessHeight;
     [SerializeField] int minStoneHeight, maxStoneHeight;
-    [SerializeField] GameObject dirt, grass, stone;
+    [SerializeField] GameObject dirt, grass, stone, diamond, ruby;
     [Range(0, 100)]
     [SerializeField] int seed;
-
+    
     // Cave Variables
     [SerializeField] int smoothCycles;
     private int[,] cavePoints;
+    private int[,] mineralPoints;
+    // randFillPercent 55 is recommended
     [Range(0, 100)]
     public int randFillPercent;
+    // threshold 4 is recommended
     [Range(0, 8)]
     public int threshold;
 
@@ -36,7 +39,6 @@ public class ProceduralGeneration : MonoBehaviour
         cavePoints = new int[width, depth];
 
         System.Random randChoice = new System.Random(seed.GetHashCode());
-        print(randChoice);
 
         for (int z = 0; z < width; z++)
         {
@@ -78,6 +80,7 @@ public class ProceduralGeneration : MonoBehaviour
         }
     }
 
+
     private int GetNeighbors(int pointZ, int pointY)
     {
         int wallNeighbors = 0;
@@ -106,7 +109,6 @@ public class ProceduralGeneration : MonoBehaviour
         return wallNeighbors;
     }
 
-
     private void Generation()
     {
         for (int z = 0; z < width; z++)
@@ -122,6 +124,7 @@ public class ProceduralGeneration : MonoBehaviour
             genHeight(z, height, totalStoneSpawnDistance);
             genDepth(z);
             genTopLevel(z, height, totalStoneSpawnDistance);
+            StartCoroutine(ExecuteAfterTime(1f, z));
 
         }
     }
@@ -141,15 +144,72 @@ public class ProceduralGeneration : MonoBehaviour
         }
     }
 
+    private IEnumerator ExecuteAfterTime(float time, int z)
+    {
+        yield return new WaitForSeconds(time);
+
+        genMinerals(z);
+    }
+
     private void genDepth(int z)
     {
+
         for (int y = 0; y < depth; y++)
         {
-            if (cavePoints[z, y] == 1)
+            int mineralGen = Random.Range(1, 101);
+            if (cavePoints[z, y] == 1 && mineralGen == 100)
+            {
+                spawnObj(diamond, -y, z);
+            }
+            else if (cavePoints[z, y] == 1 && mineralGen == 99)
+            {
+                spawnObj(ruby, -y, z);
+            }
+            else if (cavePoints[z, y] == 1)
             {
                 spawnObj(stone, -y, z);
             }
         }
+
+    }
+
+    private void genMinerals(int z)
+    {
+        int spreadDiamondsMultiplier = 0;
+
+        for (int y = 0; y < depth; y++)
+        {
+            Vector3 mineralLocation = new Vector3(0, -y, z);
+            Collider[] getBlockType = Physics.OverlapSphere(mineralLocation, 0.1f);
+            if (getBlockType.Length > 0)
+            {
+                if (getBlockType[0].gameObject.GetComponent<Diamond>())
+                {
+                    Vector3 diamondLocation = getBlockType[0].gameObject.transform.position;
+                    Collider[] diamondIsTouching = Physics.OverlapSphere(diamondLocation, .6f);
+                    if (diamondIsTouching.Length > 0)
+                    {
+                        
+                        for (int i = 0; i < diamondIsTouching.Length; i++)
+                        {
+                            if (!diamondIsTouching[i].gameObject.GetComponent<Diamond>())
+                            {
+                                GameObject newDiamond = Instantiate(diamond, diamondIsTouching[i].transform.position, Quaternion.identity);
+                                newDiamond.transform.parent = this.transform;
+                                Destroy(diamondIsTouching[i].gameObject);
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        //spreadDiamondsMultiplier++;
+        //if (spreadDiamondsMultiplier < 3)
+        //{
+        //    genMinerals(z);
+        //}
     }
 
     private void genHeight(int z, int height, int totalStoneSpawnDistance)
@@ -171,9 +231,9 @@ public class ProceduralGeneration : MonoBehaviour
     }
 
     // Spawn inside parent game object
-    private void spawnObj (GameObject obj, int width, int height)
+    private void spawnObj (GameObject obj, int y, int z)
     {
-        obj = Instantiate(obj, new Vector3(0, width, height), Quaternion.identity);
+        obj = Instantiate(obj, new Vector3(0, y, z), Quaternion.identity);
         obj.transform.parent = this.transform;
     }
 }
